585f897edd72938c8bb2c2b7f21e8e2bc4b2410e
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-14 22:24:50 +0000

    added the form submission using ajax and made post request successful

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const timeDifference = function(current, previous) {  
+const timeDifference = function (current, previous) {
   const msPerMinute = 60 * 1000;
   const msPerHour = msPerMinute * 60;
   const msPerDay = msPerHour * 24;
@@ -39,16 +39,14 @@ const timeDifference = function(current, previous) {
   }
 };
 
-const data = [
-  {
+const data = [{
     "user": {
       "name": "Ben",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirBen"
     },
     "content": {
-      "text": "Just found out that to get in a relationship I can't constantly isolate myself from everyone...lost to process."
+      "text": "Just found out that to get in a relationship I can't constantly isolate myself from everyone...lots to process."
     },
     "created_at": 1461116232227
   },
@@ -56,7 +54,8 @@ const data = [
     "user": {
       "name": "Susan",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@TheRealSusan"
+    },
     "content": {
       "text": "And in the end...the people you end up being closest to are the ones who send you memes while they're on the toilet."
     },
@@ -64,8 +63,9 @@ const data = [
   }
 ]
 
-const createTweetElement = function(tweet) {
-  const currentDate = new Date ();
+
+const createTweetElement = function (tweet) {
+  const currentDate = new Date();
   const markup = $(`
     <article class="tweet">
       <img src="${tweet.user.avatars}"<>
@@ -81,12 +81,30 @@ const createTweetElement = function(tweet) {
   return markup;
 };
 
-const renderTweets = function(tweets) {
-    for (let tweet of tweets) {
-      $("#tweet-wrapper").append(createTweetElement(tweet));
-    }
-  }; 
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    $("#tweet-wrapper").append(createTweetElement(tweet));
+  }
+};
 
-$(document).ready(function() {
-renderTweets(data);
+$(document).ready(function () {
+  renderTweets(data);
+  
+  const $form = $('#form')
+  $form.on('submit', function (event) {
+    event.preventDefault(); //prevent default action 
+    let url = $(this).attr("action"); //get form action url
+    let type = $(this).attr("method"); //get form GET/POST method
+    let data = $(this).serialize(); //Encode form elements for submission
+
+    console.log('Button clicked, performing ajax call...');
+    $.ajax({
+      url: url,
+      type: type,
+      data: data
+    }).then(function (morePostsHtml) {
+      console.log('Success', data)
+      $form.replaceWith(morePostsHtml);
+    });
+  });
 });
